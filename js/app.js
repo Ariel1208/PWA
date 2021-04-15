9d274947ea6b55628aa2ffa1de57827c418213ed
System Administrator <root@iMac-de-Carlos.local>
2021-04-15 17:35:57 -0500

    Modificamos la ruta del SW

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
+var url = window.location.href;
+var swLocation = '/twitor/sw.js';
+
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('./sw.js');
+    if (url.includes('localhost')){
+        swLocation ='./sw.js';
+    }
+    navigator.serviceWorker.register(swLocation);
 }
 
 // Referencias de jQuery
